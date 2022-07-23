cf030204bc1fe5cede686810eeb6a3a2a89e2698
David okoronkwo <77294470+david4972@users.noreply.github.com>
2022-07-23 12:03:02 -0400

    Update cbdms.py

--- cbdms.py
+++ cbdms.py
@@ -1,10 +1,12 @@
-import sqlite3  # CBDMS (CRYPTO BANK DATA MANAGEMENT SYSTEM)
+import psycopg2  # CBDMS (CRYPTO BANK DATA MANAGEMENT SYSTEM)
 
 
 # debit accounts
 def delete_debit_account_Crypto(name=str, email=str):
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
     delete = '''DELETE from CryptoDebitAccounts WHERE name=?, email=?'''
@@ -16,8 +18,10 @@ def delete_debit_account_Crypto(name=str, email=str):
 
 # all info of Debit account holders
 def all_debit_account_info_Crypto():
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
     curr.execute('''SELECT * from CryptoDebitAccounts''')
