7480f917e66f13f1ea37f4f50a065ab5ac307cfc
David okoronkwo <77294470+david4972@users.noreply.github.com>
2022-07-23 12:02:12 -0400

    Update bdms.py

--- bdms.py
+++ bdms.py
@@ -1,14 +1,15 @@
-import sqlite3  # BDMS (BANK DATA MANAGEMENT SYSTEM)
+import psycopg2  # BDMS (BANK DATA MANAGEMENT SYSTEM)
 
 
 # debit accounts
 def delete_debit_account(name=str, email=str):
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
-    delete = '''DELETE from DebitAccounts WHERE name=?, email=?'''
-    curr.execute(delete, [name, email])
+    curr.execute('''DELETE from DebitInov WHERE name=?, email=?''', [name, email])
     conn.commit()
     conn.close()
     return "account deleted"
@@ -16,13 +17,15 @@ def delete_debit_account(name=str, email=str):
 
 # all info of Debit account holders
 def all_debit_account_info():
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
-    curr.execute('''SELECT * FROM DebitAccounts''')
+    curr.execute('''SELECT * from DebitInov''')
     result1 = curr.fetchall()
-    result = 'Debit Accounts Country of Residence data'
+    result = 'Debit Accounts data'
     print(result)
     for row in result1:
         print(row)
@@ -30,11 +33,13 @@ def all_debit_account_info():
 
 # Credit Accounts
 def delete_credit_account(name=str, email=str):
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
-    delete = '''DELETE FROM CreditAccounts WHERE name=?, email=?'''
+    delete = '''DELETE FROM CreditInov WHERE name=?, email=?'''
     curr.execute(delete, [name, email])
     conn.commit()
     conn.close()
@@ -43,11 +48,13 @@ def delete_credit_account(name=str, email=str):
 
 # all info of Credit account holders
 def all_credit_account_info():
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
-    sort = curr.execute('''SELECT * FROM CreditAccounts ''')
+    sort = curr.execute('''SELECT * FROM CreditInov ''')
     result = 'Credit Account data'
     print(result)
     for row in sort:
@@ -56,13 +63,35 @@ def all_credit_account_info():
 
 # Business Credit Account
 def all_business_credit_account_info():
-    # Connecting to sqlite
-    conn = sqlite3.connect('inov.db')
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
     # Creating a cursor object using the cursor() method
     curr = conn.cursor()
-    sort = curr.execute('''SELECT * FROM B-CreditAccounts ''')
+    sort = curr.execute('''SELECT * FROM BusinessInov ''')
     result = 'Business Credit Account data'
     print(result)
     for row in sort:
         print(row)
 
+
+# International Debit Account
+def all_InternationalDebit_account_info():
+    # Connecting to postgres database server
+    conn = psycopg2.connect(
+        database="inov", user='postgres', password='', host='localhost', port='5432'
+    )
+    # Creating a cursor object using the cursor() method
+    curr = conn.cursor()
+    sort = curr.execute('''SELECT * FROM InterDebitInov ''')
+    result = 'International Debit Account data'
+    print(result)
+    for row in sort:
+        print(row)
+
+
+if __name__ == '__main__':
+    all_InternationalDebit_account_info()
+    all_credit_account_info()
+    all_debit_account_info()
